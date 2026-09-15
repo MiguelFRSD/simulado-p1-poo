72b90760f965f120d2ab91feec5f4ab6b042ab63
Miguel <mig.yt11@gmail.com>
2026-09-15 18:46:49 -0300

    o main slk se e louko

--- src/br/com/fintech/main/Main.java
+++ src/br/com/fintech/main/Main.java
@@ -1,4 +1,12 @@
 package br.com.fintech.main;
 
+import br.com.fintech.model.Transacao;
+
 public class Main {
+    public static void main (String[] args){
+        t1 = new Transacao("TX-101-BR", "PIX", 500.00, 2.50);
+        t2 = new Transacao("TX-102-BR", "CARTAO", 1200.00, 36.00);
+        t3 = new Transacao("TX-103-BR", "PIX", 350.00, 1.75);
+    }
+
 }
